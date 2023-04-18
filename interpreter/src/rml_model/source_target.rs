4d64d06fa825a7832bb691224b8dcd04674f9018
Sitt Min Oo <sittminoo1995145@gmail.com>
2023-04-18 11:45:30 +0200

    added rml logical source to algebra source conversion

--- interpreter/src/rml_model/source_target.rs
+++ interpreter/src/rml_model/source_target.rs
@@ -1,3 +1,8 @@
+use std::collections::HashMap;
+
+use algebra::{DataFormat, IOType};
+use vocab::ToString;
+
 use crate::{IriString, TermString};
 // TODO: Implement source and target metadata infos <30-03-23, Min Oo>
 
@@ -22,6 +27,17 @@ pub enum Source {
     FileInput { path: String },
 }
 
+impl Into<HashMap<String, String>> for Source {
+    fn into(self) -> HashMap<String, String> {
+        let mut map = HashMap::new();
+        match self {
+            Source::FileInput { path } => map.insert("path".to_string(), path),
+        };
+
+        map
+    }
+}
+
 #[derive(Debug, Clone)]
 pub enum FileMode {
     Append,
@@ -31,4 +47,45 @@ pub enum FileMode {
 #[derive(Debug, Clone)]
 pub enum Output {
     FileOutput { path: String, mode: FileMode },
+}
+
+fn source_config_map(ls: &LogicalSource) -> HashMap<String, String> {
+    let mut map = HashMap::new();
+
+    map.insert("identifier".to_string(), ls.identifier.to_string());
+
+    if let Some(iter) = &ls.iterator {
+        map.insert("iterator".to_string(), iter.to_owned());
+    }
+
+    let source_map: HashMap<String, String> = ls.source.clone().into();
+
+    map.extend(source_map.into_iter());
+
+    map
+}
+
+impl Into<algebra::Source> for LogicalSource {
+    fn into(self) -> algebra::Source {
+        let source_type = match &self.source {
+            Source::FileInput { path: _ } => IOType::File,
+        };
+
+        let data_format = match &self.reference_formulation.to_string() {
+            p if *p == vocab::query::CLASS::CSV.to_string() => DataFormat::CSV,
+            p if *p == vocab::query::CLASS::JSONPATH.to_string() => {
+                DataFormat::JSON
+            }
+            p if *p == vocab::query::CLASS::XPATH.to_string() => {
+                DataFormat::XML
+            }
+            p => panic!("Data format not supported {} ", p),
+        };
+
+        algebra::Source {
+            configuration: source_config_map(&self),
+            source_type,
+            data_format,
+        }
+    }
 }
