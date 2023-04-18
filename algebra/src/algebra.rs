213419b6db2bc998be2358479df87e0ae537755a
Sitt Min Oo <sittminoo1995145@gmail.com>
2023-04-18 09:43:40 +0200

    added operators for algebra

--- algebra/src/algebra.rs
+++ algebra/src/algebra.rs
@@ -1,10 +1,36 @@
 use std::collections::HashMap;
+use std::path::PathBuf;
+use std::rc::Rc;
 
 #[derive(Debug, Clone)]
-pub enum AlgebraType<T> {
-    Source(Source),
-    Mapping(Mapping<T>),
-    Target(Target),
+pub enum Operator {
+    SourceOp(Source, Rc<Operator>),
+    MappingOp(Mapping, Rc<Operator>),
+    TargetOp(Target),
+}
+
+
+#[derive(Debug, Clone)]
+pub struct Source {
+    pub configuration: HashMap<String, String>,
+    pub source_type:   IOType,
+    pub data_format:   DataFormat,
+}
+
+#[derive(Debug, Clone)]
+pub struct Mapping {
+    pub mapping_document: PathBuf,
+}
+
+#[derive(Debug, Clone)]
+pub struct Target {
+    pub configuration: HashMap<String, String>,
+    pub target_type:   IOType,
+}
+
+
+pub struct DataItem {
+    pub fields_value: HashMap<String, String>,
 }
 
 #[derive(Debug, Clone)]
@@ -25,26 +51,6 @@ pub enum IOType {
     MySQL,
     PostgreSQL,
     SPARQLEndpoint,
-}
-
-#[derive(Debug, Clone)]
-pub struct Source {
-    pub configuration: HashMap<String, String>,
-    pub source_type:   IOType,
-    pub data_format:   DataFormat,
-}
-
-#[derive(Debug, Clone)]
-pub struct Mapping<T> {
-    pub mapping_document: T,
-}
-
-
-
-#[derive(Debug, Clone)]
-pub struct Target {
-    pub configuration: HashMap<String, String>,
-    pub target_type: IOType ,
 }
 
 
