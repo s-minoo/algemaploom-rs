4d3529423c02e4f30bac4d1a44330e60dae21f10
Sitt Min Oo <sittminoo1995145@gmail.com>
2023-04-25 23:30:34 +0200

    added From traits for conversion

--- operator/src/value.rs
+++ operator/src/value.rs
@@ -15,6 +15,58 @@ pub enum Value {
     Object(HashMap<String, Value>),
 }
 
+
+
+impl From<bool> for Value{
+    fn from(value: bool) -> Self {
+        Self::Boolean(value)
+    }
+}
+
+
+impl From<String> for Value {
+    fn from(value: String) -> Self {
+        Self::String(value)
+    }
+}
+
+impl<T> From<HashMap<String, T>> for Value
+where
+    Value: From<T>,
+{
+    fn from(value: HashMap<String, T>) -> Self {
+        let val_map: HashMap<String, Value> =
+            value.into_iter().map(|(k, v)| (k, v.into())).collect();
+        Self::Object(val_map)
+    }
+}
+
+impl<T> From<Vec<T>> for Value
+where
+    Value: From<T>,
+{
+    fn from(value: Vec<T>) -> Self {
+        let val_vec: Vec<Value> = value.into_iter().map(|f| f.into()).collect();
+        Self::Array(val_vec)
+    }
+}
+
+macro_rules! from_num_val_impl {
+    ($t:ident, $number:ident) => {
+        impl From<$t> for Value {
+            fn from(value: $t) -> Self {
+                Self::Number(Number::$number(value))
+            }
+        }
+    };
+}
+
+from_num_val_impl!(f64, Double);
+from_num_val_impl!(f32, Float);
+from_num_val_impl!(u64, UInt);
+from_num_val_impl!(i64, Int);
+from_num_val_impl!(u8, Byte);
+
 #[derive(Debug, Clone, PartialEq)]
 pub enum Number {
     PosInfinity,
@@ -23,5 +75,21 @@ pub enum Number {
     Byte(u8),
     Int(i64),
     UInt(u64),
-    Float(f64),
+    Float(f32),
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_array() {
+        let arr = vec!["A", "B", "C"];
+    }
+
+    #[test]
+    fn test_object() {}
+
+    #[test]
+    fn test_number() {}
 }
