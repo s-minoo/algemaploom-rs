32b35810e183577145453d5320efbd7446c88c38
Sitt Min Oo <sittminoo1995145@gmail.com>
2023-08-24 09:50:07 +0200

    separated plan generation code

---
+++ plangenerator/src/error.rs
@@ -0,0 +1,21 @@
+use operator::Operator;
+
+#[derive(thiserror::Error, Debug)]
+pub enum PlanError {
+    #[error("Invalid to add non-leaf operator to an empty plan")]
+    EmptyPlan,
+
+    #[error("The operator {0} is not supported")]
+    OperatorNotSupported(&'static str),
+
+    #[error(
+        "The given operator cannot be added using the apply function:\n{0:?}"
+    )]
+    WrongApplyOperator(Operator),
+
+    #[error("The given operator needs to be connected to a previous opeartor: \n{0:?}")]
+    DanglingApplyOperator(Operator),
+
+    #[error("Something else happened: {0:?}")]
+    AuxError(String),
+}
