eafee7fc6e0fdcf5bdbb6c06348969eee4779558
Sitt Min Oo <sittminoo1995145@gmail.com>
2023-09-06 15:05:16 +0200

    add cleanup procedure to translator running script

--- interpreter/run_translator.sh
+++ interpreter/run_translator.sh
@@ -6,6 +6,10 @@ cd ../
 cargo b -r 
 cd interpreter 
 cargo b -r 
-../target/release/translator ./resources/tests/multiple_tm_join.ttl
-dot -Tpng ./output.dot.pretty > pretty.png
-dot -Tpng ./output.dot > output.png
+../target/release/translator ./resources/tests/multiple_tm_join.ttl 
+mkdir output/ > /dev/null  2>&1
+dot -Tpng ./output.dot.pretty > ./output/pretty.png
+dot -Tpng ./output.dot > ./output/output.png
+
+mv output.dot output/ 
+mv output.dot.pretty output/ 
