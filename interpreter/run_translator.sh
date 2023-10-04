ce9f750d827cf80a7add2a44d8d60cb8d0926e24
Sitt Min Oo <sittminoo1995145@gmail.com>
2023-10-04 14:45:23 +0200

    refactor (runscript): Change the output type from png to svg

--- interpreter/run_translator.sh
+++ interpreter/run_translator.sh
@@ -8,8 +8,8 @@ cd interpreter
 cargo b -r 
 ../target/release/translator $1
 mkdir output/ > /dev/null  2>&1
-dot -Tpng ./output.dot.pretty > ./output/pretty.png
-dot -Tpng ./output.dot > ./output/output.png
+dot -T svg ./output.dot.pretty > ./output/pretty.svg
+dot -T svg ./output.dot > ./output/output.svg
 
 mv output.dot output/ 
 mv output.dot.pretty output/ 
