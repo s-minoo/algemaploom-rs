7ca1be21efdde8815570600577f11f65ae9dc429
Sitt Min Oo <sittminoo1995145@gmail.com>
2023-10-19 10:22:35 +0200

    feat (interpreter): Add method to check for presence of ParentTriplesMap in TriplesMap

--- interpreter/src/rml_model/mod.rs
+++ interpreter/src/rml_model/mod.rs
@@ -1,7 +1,6 @@
 use self::source_target::LogicalSource;
 use self::term_map::{GraphMap, ObjectMap, PredicateMap, SubjectMap};
 
-
 pub mod join;
 pub mod source_target;
 pub mod term_map;
@@ -20,6 +19,12 @@ pub struct TriplesMap {
     pub graph_map:      Option<GraphMap>,
 }
 
+impl TriplesMap {
+    pub fn contains_ptm(&self) -> bool {
+        self.po_maps.iter().any(|pom| pom.contains_ptm())
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct PredicateObjectMap {
     pub predicate_maps: Vec<PredicateMap>,
@@ -28,7 +33,6 @@ pub struct PredicateObjectMap {
 
 impl PredicateObjectMap {
     pub fn contains_ptm(&self) -> bool {
-        self.object_maps
-            .iter().any(|om| om.parent_tm.is_some())
+        self.object_maps.iter().any(|om| om.parent_tm.is_some())
     }
 }
