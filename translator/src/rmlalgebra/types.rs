5e63faacdc4b6187944c69b4872b01c6048f79cc
Sitt Min Oo <sittminoo1995145@gmail.com>
2024-03-25 11:02:12 +0100

    feat (translator/rml): Add PartialEq trait to quads to enable  comparison

--- translator/src/rmlalgebra/types.rs
+++ translator/src/rmlalgebra/types.rs
@@ -15,6 +15,21 @@ pub struct RefPOM<'a> {
     pub om: Vec<&'a ObjectMap>,
 }
 
+impl<'a> PartialEq for RefPOM<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        let pm_identifiers_left: Vec<_> =
+            self.pm.iter().map(|pm| &pm.tm_info.identifier).collect();
+        let pm_identifiers_right: Vec<_> =
+            other.pm.iter().map(|pm| &pm.tm_info.identifier).collect();
+        let om_identifiers_left: Vec<_> =
+            self.om.iter().map(|om| &om.tm_info.identifier).collect();
+        let om_identifiers_right: Vec<_> =
+            other.om.iter().map(|om| &om.tm_info.identifier).collect();
+        pm_identifiers_left == pm_identifiers_right
+            && om_identifiers_left == om_identifiers_right
+    }
+}
+
 impl<'a> From<&'a PredicateObjectMap> for RefPOM<'a> {
     fn from(value: &'a PredicateObjectMap) -> Self {
         Self {
@@ -30,16 +45,35 @@ pub struct Quads<'a> {
     pub gms:     Vec<&'a GraphMap>,
 }
 
+impl<'a> PartialEq for Quads<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        let gm_identifiers_left: Vec<_> =
+            self.gms.iter().map(|gm| &gm.tm_info.identifier).collect();
+        let gm_identifiers_right: Vec<_> =
+            other.gms.iter().map(|gm| &gm.tm_info.identifier).collect();
+
+        self.triples == other.triples
+            && gm_identifiers_left == gm_identifiers_right
+    }
+}
+
 #[derive(Debug, Clone)]
 pub struct Triples<'a> {
     pub sm:   &'a SubjectMap,
     pub poms: Vec<RefPOM<'a>>,
 }
 
+impl<'a> PartialEq for Triples<'a> {
+    fn eq(&self, other: &Self) -> bool {
+        self.sm.tm_info.identifier == other.sm.tm_info.identifier
+            && self.poms == other.poms
+    }
+}
+
+pub type TMPlanPair<'a> = (&'a TriplesMap, Rc<RefCell<Plan<Processed>>>);
 #[derive(Debug, Clone)]
 pub struct SearchMap<'a> {
-    pub tm_rccellplan_map:
-        HashMap<String, (&'a TriplesMap, Rc<RefCell<Plan<Processed>>>)>,
+    pub tm_rccellplan_map:  HashMap<String, TMPlanPair<'a>>,
     pub variable_map:       HashMap<String, String>,
     pub target_map:         HashMap<String, Target>,
     pub lt_id_tm_group_map: HashMap<String, Vec<Quads<'a>>>,
