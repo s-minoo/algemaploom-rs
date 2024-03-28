6f60e3b679ea2d1e48937ce5490626ec029cf390
Sitt Min Oo <sittminoo1995145@gmail.com>
2024-03-28 13:40:07 +0100

    feat (translator/shexml): Handles the translation of nested iterators for rename map

--- translator/src/shexml/operators/rename.rs
+++ translator/src/shexml/operators/rename.rs
@@ -1,8 +1,31 @@
 use std::collections::HashMap;
 
-use shexml_interpreter::{
-    ExpressionStmt, FieldType, Iterator,
-};
+use shexml_interpreter::{ExpressionStmt, FieldType, Iterator};
+
+fn update_rename_map_iterator(
+    prefix: &str,
+    iterator: &Iterator,
+    rename_pairs: &mut HashMap<String, String>,
+) {
+    let normal_fields = iterator
+        .fields
+        .iter()
+        .filter(|field| field.field_type == FieldType::Normal);
+
+    normal_fields.for_each(|field| {
+        let from = field.ident.to_string();
+        let to = format!("{}.{}", prefix, field.ident);
+        rename_pairs.insert(from, to);
+    });
+
+    for nested_iter in iterator.nested_iterator.iter() {
+        update_rename_map_iterator(
+            &format!("{}.{}", prefix, nested_iter.ident),
+            nested_iter,
+            rename_pairs,
+        );
+    }
+}
 
 pub fn translate_rename_pairs_map(
     iterators_map: &HashMap<String, Iterator>,
@@ -16,23 +39,13 @@ pub fn translate_rename_pairs_map(
         let expr_ident = &expr_stmt.ident;
 
         if let Some(field) = &reference.field {
-            let from = format!("{}.{}", iter_ident, field);
+            let from = field.to_string();
             let to = format!("{}.{}", expr_ident, field);
 
             rename_pairs.insert(from, to);
         } else if let Some(iterator) = iterators_map.get(iter_ident) {
-            let normal_fields = iterator
-                .fields
-                .iter()
-                .filter(|field| field.field_type == FieldType::Normal);
-
-            normal_fields.for_each(|field| {
-                let from = format!("{}.{}", iter_ident, field.ident);
-                let to = format!("{}.{}", expr_ident, field.ident);
-                rename_pairs.insert(from, to);
-            })
+            update_rename_map_iterator(expr_ident, iterator, &mut rename_pairs);
         }
     }
-
     rename_pairs
 }
